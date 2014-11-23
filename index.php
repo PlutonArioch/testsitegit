c333b1c25d995642a07a2ddfb46ecdeb51a43779
PlutonArioch <plutonarioch@gmail.com>
2014-11-23 18:01:27 +0100

    minimal testing index.php and.htaccess

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+<header>
+<title>Testsite</title>
+</header>
+<body bgcolor="#111111" text="#00ff00" alink=#0000ff vlink=#ff00ff>
+<?php
+echo "test.php\n\n<br>I Got:<br>";
+echo $_SERVER['REQUEST_URI'];
+?>
+
+</body>
+</html>
